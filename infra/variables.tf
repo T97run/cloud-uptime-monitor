5bbe83cc319a642f62a075282c2db96ae6c70128
Tarun Mistry <tarunmistry@gmail.com>
2025-09-08 07:43:17 -0400

    minor changes

--- infra/variables.tf
+++ infra/variables.tf
@@ -15,6 +15,11 @@ variable "project_name" {
 variable "urls" {
   type        = list(string)
   description = "List of URLs to check"
+  default = [
+"https://example.com",
+"https://httpbin.org/get",
+"https://google.com"
+]
 }
 
 
